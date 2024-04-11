6786c35b2492a321b8228a15c14d365c535b9da2
gholly <gholly520@gmail.com>
2024-04-11 14:42:00 +0800

    nacos

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.BeansException;
@@ -8,6 +12,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/test")
 @RequiredArgsConstructor
@@ -24,6 +30,20 @@ public class TestController implements ApplicationContextAware {
     @GetMapping("/shut-down")
     public void shutDown() {
         ((ConfigurableApplicationContext) context).close();
+    }
+
+    @NacosInjected
+    private NamingService namingService;
+
+    /**
+     * 根据服务名称获取服务的所有（机器）实例信息
+     * @param serviceName 服务名称
+     * @return
+     * @throws NacosException
+     */
+    @GetMapping("getInstances")
+    public List<Instance> getInstances(String serviceName) throws  NacosException {
+        return namingService.getAllInstances(serviceName);
     }
 
     @GetMapping("/say-hi")
